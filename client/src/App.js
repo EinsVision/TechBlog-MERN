26c149f276971ca23a2bef4ff65636904bedb7a7
Changdae <einsvision@gmail.com>
2021-03-12 11:22:26 +0900

    making JSX strutures using Container and AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello MERN!✨</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Technology Memo
+        </Typography>
+        <img 
+          src='https://www.rashaunforrest.com/assets/img/frontend.png' 
+          alt='memories'
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+      
+    </Container>
   );
 }
 
